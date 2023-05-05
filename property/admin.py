3e2a68070f1d36245c77fda2bac0e976fa1a03aa
baiv84 <iambabintsev@gmail.com>
2023-05-05 16:10:50 +0700

    add field Flat.owner_pure_phone

--- property/admin.py
+++ property/admin.py
@@ -5,9 +5,9 @@ from .models import UserProfile
 
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('town', 'address', 'owner', )
+    search_fields = ('town', 'address', 'owner', 'owner_pure_phone', )
     readonly_fields = ('created_at', )
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town',)
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', 'owner_pure_phone', )
     list_editable = ('new_building', )
     list_filter = ('new_building', 'rooms_number', 'has_balcony', )
     raw_id_fields = ('liked_by',)
